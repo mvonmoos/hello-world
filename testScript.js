25fa92ec73f538a7f260bf6ba05c0d8c28203831
mvonmoos <mvm@magno.ch>
2018-06-06 17:12:40 +0200

    asdas
    c hange script

--- testScript.js
+++ testScript.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $("button").click(function(){
-        $("#div1").fadeToggle();
+        $("#mainPanel").fadeToggle();
         $("#div2").fadeToggle("slow");
         $("#div3").fadeToggle(3000);
     });
