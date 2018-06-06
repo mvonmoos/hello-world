1b2b89ffd00b74241085d6aa398657908d837989
mvonmoos <mvm@magno.ch>
2018-06-06 16:57:04 +0200

    remove shiot from the script

--- testScript.js
+++ testScript.js
@@ -1,12 +1,6 @@
 $(document).ready(function(){
-    $("button").click(function(){
-        $("#MAKETHESUPERCHANGE").fadeToggle();
-        $("#MAKETHESUPERCHANGE").fadeToggle("slow");
-        $("#MAKETHESUPERCHANGE").fadeToggle(3000);
-    });
-	
 	$("experimental").click(function(){
         alert('hello');
-		alert('hello2');
+	alert('hello2');
     });
 });
