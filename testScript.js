9d98dcfe6c5a3e4c52422439387021df99586048
mvonmoos <mvm@magno.ch>
2018-06-06 16:39:11 +0200

    changes

--- testScript.js
+++ testScript.js
@@ -4,4 +4,10 @@ $(document).ready(function(){
         $("#MAKETHESUPERCHANGE").fadeToggle("slow");
         $("#MAKETHESUPERCHANGE").fadeToggle(3000);
     });
+	
+	$("experimental").click(function(){
+        $("#123").fadeToggle();
+        $("#123").fadeToggle("slow");
+        $("#123").fadeToggle(3000);
+    });
 });
