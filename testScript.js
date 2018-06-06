82828a32b189085ea37df9bc3ffad421239c7782
mvonmoos <mvm@magno.ch>
2018-06-06 16:15:11 +0200

    wrong selector ins cript

--- testScript.js
+++ testScript.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $("button").click(function(){
-        $("#div1").fadeToggle();
-        $("#div2").fadeToggle("slow");
-        $("#div3").fadeToggle(3000);
+        $("#MAKETHESUPERCHANGE").fadeToggle();
+        $("#MAKETHESUPERCHANGE").fadeToggle("slow");
+        $("#MAKETHESUPERCHANGE").fadeToggle(3000);
     });
 });
